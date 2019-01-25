6467e9a8e49ccf6a96da82c813fa8c270f1a0857
darya <kokorina.darya1509@gmail.com>
2019-01-25 15:03:58 +0300

    umd#2

--- eventCalendar.js
+++ eventCalendar.js
@@ -1,18 +1,20 @@
-(function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(['./EventCalendar'], factory);
-  } else if (typeof exports === 'object') {
+(function(global, factory) {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
     // CommonJS
-    module.exports = factory(require('./EventCalendar'));
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(factory);
   } else {
-    // Browser globals (Note: root is window)
-    global.returnExports = factory(global.EventCalendar);
+    // Глобальная переменная (если скрипт выполняется в браузере, тогда это будет `window`)
+    global.EventCalendar = factory();
   }
-}(this, function (EventCalendarModule) {
+})(this, function() {
   'use strict';
 
-  var EventCalendar = (function () {
+  function EventCalendar () {
+
+  }
 
     function getAllEvents() {
       return JSON.parse(window.localStorage.getItem('events')) || [];
@@ -56,15 +58,18 @@
 
     runEventTimer();
 
-    var Constructor = function EventCalendar() {
+    function getEventsByInterval(startTime, endTime) {
+      return events.filter(function (event) {
+        var dateEvent = new Date(toMsConverter(event.date));
+        return dateEvent.getTime() >= startTime.getTime() && dateEvent.getTime() <= endTime.getTime();
+      });
+    }
 
-    };
-
-    Constructor.prototype.logEvents = function () {
+  EventCalendar.prototype.logEvents = function () {
       console.log(events);
     };
 
-    Constructor.prototype.setEvent = function (titleEvent, dateEvent, callback) {
+  EventCalendar.prototype.setEvent = function (titleEvent, dateEvent, callback) {
       var event = {
         title: titleEvent,
         date: dateEvent,
@@ -77,25 +82,57 @@
 
     };
 
-    Constructor.prototype.getDayEvents = function () {
+  EventCalendar.prototype.getDayEvents = function () {
       var date = new Date();
       var year = date.getFullYear();
       var month = date.getMonth();
       var day = date.getDate();
-      return events.map(function (event) {
+      return events.filter(function (event) {
         var dateEvent = new Date(toMsConverter(event.date));
         return dateEvent.getFullYear() === year && dateEvent.getMonth() === month && dateEvent.getDate() === day;
       });
     };
 
-    Constructor.prototype.deleteEvent = function (id) {
+  EventCalendar.prototype.getWeekEvents = function () {
+      var date = new Date();
+      var currentDay = date.getDay();
+      var startTime = new Date();
+      if (currentDay === 0) {
+        startTime.setDate(date.getDate() - startTime.getDay() - 6);
+      } else {
+        startTime.setDate(date.getDate() - startTime.getDay() + 1);
+      }
+      var endTime = new Date();
+      if (currentDay === 0) {
+        endTime.setDate(date.getDate());
+      } else {
+        endTime.setDate(date.getDate() + 7 - endTime.getDay());
+      }
+      return getEventsByInterval(startTime, endTime);
+    };
+
+  EventCalendar.prototype.daysInMonth = function (month, year) {
+      return new Date(year, month, 0).getDate();
+    };
+
+  EventCalendar.prototype.getMonthEvents = function () {
+      var date = new Date();
+      var startTime = new Date();
+      startTime.setDate(1);
+
+      //здесь останивилась я!!!
+
+      return getEventsByInterval(startTime, endTime);
+    };
+
+  EventCalendar.prototype.deleteEvent = function (id) {
       events = events.map(function (event) {
         return event.id !== id;
       });
       updateLocalStorage(events);
     };
 
-    Constructor.prototype.changeEventTitle = function (id, newTitle) {
+  EventCalendar.prototype.changeEventTitle = function (id, newTitle) {
       events = events.map(function (event) {
         if (event.id === id) {
           event.title = newTitle;
@@ -105,7 +142,7 @@
       updateLocalStorage(events);
     };
 
-    Constructor.prototype.changeEventDate = function (id, newDate) {
+  EventCalendar.prototype.changeEventDate = function (id, newDate) {
       events.forEach(function (item) {
         if (item.id === id) {
           item.date = newDate;
@@ -114,10 +151,9 @@
       updateLocalStorage(events);
     };
 
-    return Constructor;
-  })();
-var calendar = new EventCalendar();
-}));
+  return new EventCalendar();
+
+});
 
 
 
